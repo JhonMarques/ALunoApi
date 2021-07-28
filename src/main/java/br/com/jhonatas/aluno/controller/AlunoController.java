ce57644b8ac40ac83e5b492567364db19a14f895
JhonMarques <jhonatasbcc@gmail.com>
2021-07-28 14:53:16 -0300

    swagger implementado

--- src/main/java/br/com/jhonatas/aluno/controller/AlunoController.java
+++ src/main/java/br/com/jhonatas/aluno/controller/AlunoController.java
@@ -3,6 +3,8 @@ package br.com.jhonatas.aluno.controller;
 import br.com.jhonatas.aluno.handler.request.AlunoNotFoundException;
 import br.com.jhonatas.aluno.model.Aluno;
 import br.com.jhonatas.aluno.service.AlunoService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -10,8 +12,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/aluno")
 @AllArgsConstructor
+@RequestMapping("/aluno")
+@Api(value="API REST Alunos")
+@CrossOrigin(origins ="*")
 public class AlunoController {
 
     private AlunoService alunoService;
@@ -19,27 +23,32 @@ public class AlunoController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping(value = "/novo")
+    @ApiOperation(value="Cadastra um novo aluno")
     public Aluno create(@RequestBody Aluno aluno) {
         return alunoService.create(aluno);
     }
 
     @GetMapping(value = "/listarTodos")
+    @ApiOperation(value="Retorna uma lista de alunos")
     public List<Aluno> listAll() {
         return alunoService.findAll();
     }
 
     @GetMapping(value = "/procurarPorId/{id}")
+    @ApiOperation(value="Retorna um aluno pelo ID")
     public Aluno findById(@PathVariable Integer id) throws AlunoNotFoundException {
         return alunoService.findById(id);
     }
 
     @PutMapping(value = "/atualizarAluno/{id}")
+    @ApiOperation(value="Atualiza um aluno cadastrado")
     public Aluno updateById(@PathVariable Integer id, @RequestBody Aluno aluno) throws AlunoNotFoundException {
         return alunoService.updateById(id, aluno);
     }
 
     @DeleteMapping(value = "/deletar/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
+    @ApiOperation(value="Exclui um aluno")
     public void DeleteById(@PathVariable Integer id) throws AlunoNotFoundException {
         alunoService.delete(id);
 
